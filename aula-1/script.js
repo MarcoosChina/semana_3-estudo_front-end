663ea7b52cfa28ff35a509ede1355c9d6419742a
estudostirodrigo-debug <estudostirodrigo@gmail.com>
2026-07-15 19:02:21 -0300

    second commit

---
+++ aula-1/script.js
@@ -0,0 +1,18 @@
+/*Semana 2 SCTEC Desenvolvimento de Software Front-End
+
+//PRIMEIRO EXERCÍCIO
+
+//var nome = 'Rodrigo de Souza';
+
+//console.log('Tamanho do nome:', nome.length);  // length é um atributo que retorna o tamanho da string
+
+//console.log(nome.split(' ')[0]); // Imprime o primeiro nome  //split() é um método que divide a string em um array de substrings, usando o espaço como delimitador
+//console.log(nome.split(' ')[2]); // Imprime o último nome 
+*/
+
+//SEGUNDO EXERCÍCIO
+
+var cidade = 'Cidade de São Paulo';
+
+console.log('Nome Maiusculo:', cidade.toUpperCase());
+console.log('Nome minúsculo:', cidade.toLowerCase());
